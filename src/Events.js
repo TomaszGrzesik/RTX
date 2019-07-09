2f452127ee6b3f7c08dc3d8963b8148b374ae14b
lukwyp <lukasz.wpvp@gmail.com>
2019-07-09 19:55:28 +0200

    Updated

--- src/Events.js
+++ src/Events.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import EventItem from './EventItem';
+import Chart from './Chart';
 
 class Events extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { events: [] };
+    this.state = { 
+      events: [],
+      ids: []
+     };
+  }
+
+  onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
   }
 
   componentDidMount() {
-    fetch(`http://localhost:3000/frames`)
+    fetch(`https://frames.free.beeceptor.com/`)
     .then(response => response.json())
     .then(data => {
+      const uniqueTags = [];
+      data.map(item => {
+        if (uniqueTags.indexOf(item.frame_id) === -1) {
+          uniqueTags.push(item.frame_id)
+        }
+      });
         this.setState({
-      events: data
+      events: data,
+      ids: uniqueTags
     });
   });
   }
@@ -23,22 +37,39 @@ class Events extends React.Component {
     this.setState({ events: [] });
   }
 
+  getChartData(id){
+      const tempData = this.state.events.filter(f=>f.frame_id === id);
+      const chartData = {
+        name: tempData.map(m=>m.data).map(m=>Object.keys(m)[0])[0],
+        labels: tempData.map(m=>m.timestamp),
+        datasets:[
+          {
+            label: id,
+            data: tempData.map(m=>m.data).map(m=>Object.values(m)[0]),
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      };
+
+      return chartData;
+    }
 
   render() {
     return (
       <div>
         <ul>
-          {this.state.events.map(item => {
-
-              return (
-                <EventItem {...item} key={item.id} />
-              );
-
-
-            //return null;
-          })}
+          {this.state.ids.map(item=>
+            {return (<Chart chartData={this.getChartData(item)} legendPosition="bottom" key={item}/>);})
+          } 
         </ul>
-
       </div>
     );
   }
